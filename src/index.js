7f2bcd8d9a2e50cb4911fa8f0d1a767d747c8c05
Isabelle Maia <isabelle.silva@bitcapital.com.br>
2022-02-02 01:18:34 -0300

    rockeseat-ignite: rotasHTTP

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
-const { response } = require('express');
-const express = require ('express');
+//const { response } = require('express');
+//const express = require ('express');
 
 const app = express();
 
 //localhost://3333
 
-app.get("/", (request, response)=>{
-    return response.json({ message: "Hello World"});
+app.get("/Courses", (request, response)=>{
+    return response.json( [
+        "Curso1",
+        "Curso2",
+        "Curso3"
+    ]);
 
-})
+});
 
+app.post("/Courses", (request, response)=>{
+    return response.json( [
+        "Curso1",
+        "Curso2",
+        "Curso3",
+        "Curso4"
+    ]);
+
+});
+
+app.put("/Courses:id", (request, response)=>{
+    return response.json( [
+        "Curso6",
+        "Curso2",
+        "Curso3",
+        "Curso4"
+    ]);
+
+});
+
+app.path("/Courses:id", (request, response)=>{
+    return response.json( [
+        "Curso6",
+        "Curso7",
+        "Curso3",
+        "Curso4"
+    ]);
+
+});
+
+app.delete("/Courses:id", (request, response)=>{
+    return response.json( [
+        "Curso6",
+        "Curso7",
+        "Curso4"
+    ]);
+
+});
 
 app.listen(3333);
